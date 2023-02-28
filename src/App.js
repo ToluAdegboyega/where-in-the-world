7554beb98a9127729113a52ccff2e8bb608c8516
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2023-02-28 17:27:02 +0100

    chore: routing, theme switching and reusable  styles.

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./App.scss";
+
+import Country from "./views/Country";
+import Error from "./views/Error";
+import Home from "./views/Home";
+import Header from "./components/Header";
+
+function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://restcountries.com/v2/all")
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.message) {
+          throw new Error(json.message);
+        } else {
+          setData(json);
+          setLoading(false);
+        }
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="app min-h-screen grid">
+      <Router>
+        <Header />
+        {!loading ? (
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                !error ? data && <Home data={data} /> : <Error error={error} />
+              }
+            />
+            {data &&
+              data.map((country) => {
+                return (
+                  <Route
+                    exact
+                    path={`/${country.alpha3Code}`}
+                    key={country.alpha3Code}
+                    element={<Country country={country} data={data} />}
+                  />
+                );
+              })}
+            <Route exact path="/:code" element={<Error error={error} />} />
+          </Routes>
+        ) : (
+          <div className="flex justify-center items-center">
+            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500 animate-spin">
+              <div className="h-9 w-9 rounded-full bg-white"></div>
+            </div>
+          </div>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Moon } from "../assets/icons/moon.svg";
+import { ReactComponent as MoonSolid } from "../assets/icons/moon-solid.svg";
+
+const Header = () => {
+  const light = "theme__light";
+  const dark = "theme__dark";
+
+  let html = document.documentElement;
+
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || light);
+
+  const switchTheme = () => {
+    if (theme === dark) {
+      setTheme(light);
+    } else {
+      setTheme(dark);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    html.className = theme;
+  }, [html, theme]);
+
+  return (
+    <div className="header h-20 w-100 flex justify-between items-center py-0 md:px-20 px-6">
+      <h1 className="md:text-2xl text-base font-semibold cursor-pointer">
+        <Link to="/" className="px-0 py-2">
+          Where In The World?
+        </Link>
+      </h1>
+
+      <button
+        onClick={switchTheme}
+        className="flex justify-end items-center 
+        h-12 py-0 px-4 -mr-4 text-xs font-semibold cursor-pointer border-none bg-transparent rounded-md"
+      >
+        {theme === light ? <MoonSolid className="h-5 m-2" /> : <Moon className="h-5 m-2" />}
+        {theme === light ? "Dark" : "Light"} Mode
+      </button>
+    </div>
+  );
+};
+
+export default Header;
